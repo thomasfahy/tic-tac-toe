6ba2126ce07a2aae443e62ad0a11f0f723ee6145
thomasfahy <thomasfahy11@gmail.com>
2024-09-10 19:52:47 +0100

    feat: adding tictactoe logic to js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const TicTacToeBoard = () => {
         ['', '', '']
     ];
 
-    let placeMark = (row, col, mark) => {
+    let placeMark = (row, col, player) => {
         if (gameBoard[row][col] === ''){
             gameBoard[row][col] = mark;
             return true;
@@ -28,8 +28,32 @@ const TicTacToeBoard = () => {
             [ [0, 0], [1, 1], [2, 2] ],
             [ [0, 2], [1, 1], [2, 0] ]
         ];
+        
+        function checkWinner(gameBoard, player) {
+            for (let i=0; i > winningCombinations.length; i++){
+                const combo = winningCombinations[i];
+                
+                if (combo.every(([row, col]) => gameBoard[row][col] === player)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        if (checkWinner(gameBoard, 'X')) {
+            console.log("Player X wins!");
+        }else if (checkWinner(gameBoard, 'O')) {
+            console.log("Player O wins!");
+        } else {
+            console.log("No winner yet!");
+        }
+
     }
 
+    function player(name, symbol){
+        this.name = name;
+        this.symbol = symbol;
+    }
 
     return {
         placeMark,
