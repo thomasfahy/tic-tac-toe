07903bbc686653257ed00eb4c2decc22329d512f
thomasfahy <thomasfahy11@gmail.com>
2024-09-10 21:21:52 +0100

    feat: add basic HTML CSS and advanced js script

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
-function player(name, mark){
-    this.name = name;
-    this.mark = mark;
+const Gameboard = (() => {
+    let gameboard = ["","","","","","","","",""];
+
+    const drawBoard = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+        })
+    document.querySelector('.gameboard').innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square")
+    console.log(squares);
+    squares.forEach((square) => {
+        square.addEventListener("click", Game.handleClick);
+    })
+    }
+
+
+    return {
+        drawBoard,
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
 }
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.drawBoard();
+    }
+
+    const handleClick = (event) => {
+        console.log(event.target.id);
+    }
+    return{
+        start,
+        handleClick
+    }
+})();
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    Game.start();
+})
